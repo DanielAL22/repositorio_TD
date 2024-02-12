c3ba17e7c220d437ac0cca3eaf93aac89cdebe73
Daniel Almería <daniel.alm92@gmail.com>
2024-02-12 19:04:54 -0300

    subida version mejorada viajes

--- prueba_evaluada_viajes_chile_daniel_almeria/assets/js/script.js
+++ prueba_evaluada_viajes_chile_daniel_almeria/assets/js/script.js
@@ -10,14 +10,20 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 
 
-    /*Esta función hace que al clicar el botón de confirmar envío se muestre un toast donde se hace la confirmación*/
+    /*Esta función hace que al clicar el botón de confirmar envío se muestre un toast donde se hace la confirmación y se limpia el formulario*/
     $("#confirmarEnvio").click(function () {
         // Aquí debería ir el código del envio de datos
 
+
+
+        //Esta función visualiza el toast de exito en el envío
         $("#toastExito").toast('show');
+        //Esta función resetea el formulario
+        $("#idFormulario")[0].reset();
     });
 
 
+    /*Esta función genera el smooth scroll*/
     $('a[href^="#"]').on('click', function (event) {
         // Asegúrate de que this.hash tenga un valor antes de anular el comportamiento predeterminado
         if (this.hash !== '') {
@@ -38,4 +44,81 @@ $(document).ready(function () {
     });
 
 
+    var formularioValidado = $('#idFormulario').validate({
+
+        // Specify validation rules
+
+        rules: {
+
+            // The key name on the left side is the name attribute
+
+            // of an input field. Validation rules are defined
+
+            // on the right side
+
+            nombre: {
+                required: true,
+                pattern: /^[a-zA-Z]+$/
+            },
+            apellido: {
+                required: true,
+                pattern: /^[a-zA-Z]+$/
+            },
+
+            mensaje: "required",
+        },
+
+        // Specify validation error messages
+
+        messages: {
+
+            nombre: {
+                required: "Por favor, introduzca su nombre",
+                pattern: "Por favor, ingrese solo letras en el nombre"
+            },
+            apellido: {
+                required: "Por favor, introduzca su apellido",
+                pattern: "Por favor, ingrese solo letras en el apellido"
+            },
+            mensaje: "Por favor, introduzca su mensaje",
+
+        },
+
+
+        invalidHandler: function (event, validator) {
+            // Deshabilitar el botón de envío si hay errores
+            $('#confirmarEnvio1').prop('disabled', true);
+        },
+        submitHandler: function (form) {
+            console.log("Formulario válido. Enviando...");
+            form.submit();
+
+        }
+
+    });
+    // Deshabilitar el botón de envío al cargar la página
+    $('#confirmarEnvio1').prop('disabled', true);
+
+    // Monitorear cambios en el formulario
+    var valorNombre = $('#idNombre').val();
+    var valorApellido = $('#idApellido').val();
+    var valorMensaje = $('#idMensaje').val();
+
+    console.log('Nombre:', valorNombre);
+    console.log('Apellido:', valorApellido);
+    console.log('Mensaje:', valorMensaje);
+
+    $('#idFormulario input').on('input', function () {
+        // Verificar si el formulario es válido
+        if (formularioValidado.form()) {
+            // Habilitar el botón de envío si el formulario es válido
+            $('#confirmarEnvio1').prop('disabled', false);
+        } else {
+            // Deshabilitar el botón de envío si el formulario no es válido
+            $('#confirmarEnvio1').prop('disabled', true);
+        }
+    });
+
+
 });
+
